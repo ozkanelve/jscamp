bb3d5cfae9042a9a1cc3d957a4a5d280fc21f22a
ByPatriot <ByPatriot@DESKTOP-K122GBK>
2021-10-19 14:13:41 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Birader")
+
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"]
+
+console.log("<ul>")
+for(let i = 0; i<konutKredileri.length; i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
